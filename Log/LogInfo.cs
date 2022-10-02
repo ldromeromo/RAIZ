2c60cb819a37ad24a0ef03ee5da6fcccb81dbf06
ldromeromo <2017.903romeromld@gmail.com>
2022-10-01 20:44:46 -0500

    ADD log INFO - log ERROR

---
+++ Log/LogInfo.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Log
+{
+    public class LogInfo
+    {
+        public int AgregarLogInfo(ref string error, string proyecto)
+        {
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["STC_NUEVA_MALLA"].ToString();
+                int idSalida = default;
+
+                using (SqlConnection cn = new SqlConnection(conexion))
+                {
+                    cn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SP_LOG_INFO", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@PROCESO", 1);
+                    cmd.Parameters.AddWithValue("@PROYECTO", proyecto);
+                    cmd.Parameters.AddWithValue("@ID_PROCESO", 0);
+
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read())
+                        {
+                            idSalida = Convert.ToInt32(rdr["id"]);
+                        }
+                    }
+
+                    cn.Close();
+
+                    return idSalida;
+                }
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                throw;
+            }
+        }
+
+        public void ActualizarLogInfo(ref string error, int id)
+        {
+            try
+            {
+                string conexion = ConfigurationManager.ConnectionStrings["STC_NUEVA_MALLA"].ToString();
+                using (SqlConnection cn = new SqlConnection(conexion))
+                {
+                    cn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SP_LOG_INFO", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@PROCESO", 2);
+                    cmd.Parameters.AddWithValue("@PROYECTO", string.Empty);
+                    cmd.Parameters.AddWithValue("@ID_PROCESO", id);
+                    cmd.ExecuteReader();
+
+                    cn.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                error = e.Message;
+                throw;
+            }
+        }
+    }
+}
